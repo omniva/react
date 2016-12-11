cc007ebe62e49cee9bb13fef51ae5d6e1ea1397d
Omniva <heinsouman@MacBook-Air-van-Hein.local>
2016-12-11 21:58:24 +0100

    dutch support

--- app/i18n.js
+++ app/i18n.js
@@ -9,17 +9,21 @@ import { DEFAULT_LOCALE } from '../app/containers/App/constants';
 
 import enLocaleData from 'react-intl/locale-data/en';
 import deLocaleData from 'react-intl/locale-data/de';
+import nlLocaleData from 'react-intl/locale-data/nl';
 
 addLocaleData(enLocaleData);
 addLocaleData(deLocaleData);
+addLocaleData(nlLocaleData);
 
 export const appLocales = [
   'en',
   'de',
+  'nl',
 ];
 
 import enTranslationMessages from './translations/en.json';
 import deTranslationMessages from './translations/de.json';
+import nlTranslationMessages from './translations/nl.json';
 
 export const formatTranslationMessages = (locale, messages) => {
   const defaultFormattedMessages = locale !== DEFAULT_LOCALE ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages) : {};
@@ -39,4 +43,5 @@ export const formatTranslationMessages = (locale, messages) => {
 export const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
   de: formatTranslationMessages('de', deTranslationMessages),
+  nl: formatTranslationMessages('nl', nlTranslationMessages),
 };
